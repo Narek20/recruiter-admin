08e3f344cfd691940736a01473f3fa6ca23908c4
Narek <narek.hovhannisyan@stdevmail.com>
2026-03-12 19:47:51 +0400

    new change

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,6 +2,6 @@ import { http } from "../lib/http";
 import { LoginPayload, LoginResponse } from "../types/auth";
 
 export async function loginRequest(payload: LoginPayload) {
-  const { data } = await http.post<LoginResponse>("/auth/login", payload);
+  const { data } = await http.post<LoginResponse>("/auth/admin/login", payload);
   return data;
 }
